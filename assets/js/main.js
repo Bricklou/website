7ae254176429b959b63f0dc1dbe72c4d97fb4b22
Louis Bailleau <louis.bailleau@free.fr>
2023-11-03 21:02:06 +0100

    feat: add 404 page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
-import { parseEmojis } from './emojis';
-import { parseHeaders } from './headers-links';
-import { imageZoom } from './image-zoom';
-import { highlightCode } from './highlight-code';
+import { parseEmojis } from "./emojis";
+import { parseHeaders } from "./headers-links";
+import { imageZoom } from "./image-zoom";
+import { highlightCode } from "./highlight-code";
 
-document.addEventListener('DOMContentLoaded', function () {
-  console.log('DOMContentLoaded');
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOMContentLoaded");
   parseHeaders();
   parseEmojis();
   imageZoom();
